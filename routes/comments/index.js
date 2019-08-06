13fc90c7add3220f374b2d3638557fd0183f9de5
myke47 <mychel.larson@gmail.com>
2019-08-05 17:21:37 -0700

    Endpoints designed, begin testing

--- routes/comments/index.js
+++ routes/comments/index.js
@@ -17,6 +17,26 @@ commentsRouter.get('/api/posts/:id/comments', async (req, res) => {
 });
 
 //POST 'insert' new comment to post
+commentsRouter.post('/api/posts/:id/comments', async (req, res) => {
+  const post_id = req.params.id;
+  const { text } = req.body;
+
+  if (!text) {
+    res.status(400).json({ success: false, errorMessage: 'Please provide text for the comment.' });
+  } else {
+    await Posts.insertComment({ text, post_id })
+    .then(comment => {
+      if (comment) {
+        res.status(200).json({ success: true, comment });
+      } else {
+        res.status(404).json({ success: false, message: 'The post with the specified ID does not exist.'});
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, errorMessage: `There was an error while saving the comment to the database. post id: ${req.params.id} comment text: ${req.body.text} error: ${error}`});
+    });
+  }
+});
 
 
 module.exports = commentsRouter;
--- routes/posts/index.js
+++ routes/posts/index.js
@@ -12,7 +12,57 @@ postsRouter.get('/api/posts', async (req, res) => {
   });
 });
 
-//GET Posts by id TODO
+//GET Posts by ID TODO
+postsRouter.post('/api/posts', async (req, res) => {
+  const { title, contents } = req.body;
 
+  if (!title || !contents) {
+    res.status(400).json({ success: false, errorMessage: 'Please provide title and contents for the post.' });
+  } else {
+    await Posts.insert(req.body)
+    .then(post => {
+      res.status(201).json({ success: true, post });
+    })
+    .catch(() => {
+      res.status(500).json({ success: false, errorMessage: 'There was an error while saving the post to the database.' });
+    });
+  }
+});
+
+//PUT 'update' post by ID
+postsRouter.put('/api/posts/:id', async (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res.status(400).json({ success: false, errorMessage: 'Please provide the title and contents for the post.'});
+  } else {
+    await Posts.update(req.params.id, { title, contents })
+    .then(post => {
+      if (post) {
+        res.status(200).json({ success: true, post: { postTitle: title, postsContents: contents } });
+      } else {
+        res.status(404).json({ success: false, message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ success: false, errorMessage: 'The information could not be modified.' });
+    });  
+  }
+});
+
+//DESTROY 'delete'
+postsRouter.delete('/api/posts/:id', async (req, res) => {
+  await Posts.remove(req.params.id)
+  .then(count => {
+    if (count && count > 0) {
+      res.status(200).json({ success: true, message: `You have deleted the post with id: ${req.params.id}`});
+    } else {
+      res.status(404).json({ success: false, message: 'The post with the specified ID does not exist.' });
+    }
+  })
+  .catch(() => {
+    res.status(500).json({ success: false, errorMessage: 'The post could not be removed.' });
+  });
+});
 
 module.exports = postsRouter;
